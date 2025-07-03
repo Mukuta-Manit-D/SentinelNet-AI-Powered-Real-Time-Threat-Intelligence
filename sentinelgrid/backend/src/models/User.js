1954a894eb0931a24db4e0021d6e432415a3142c
Mukuta Manit D <dineshsairam1919@gmail.com>
2025-07-03 11:58:32 +0530

    Add files via upload

--- sentinelgrid/backend/src/models/User.js
+++ sentinelgrid/backend/src/models/User.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  role: { type: String, default: 'attendee' }
-}, { timestamps: true });
+  name: String,
+  email: { type: String, unique: true },
+  role: String,
+  password: String,
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Add a method to compare passwords
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
